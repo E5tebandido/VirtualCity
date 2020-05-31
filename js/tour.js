767de6517b4295e0a9226a9b939f2aec1bc67535
Esteban Posada <aepr_18_@hotmail.com>
2020-05-31 02:54:09 -0500

    Added sound

--- js/tour.js
+++ js/tour.js
@@ -2,3 +2,10 @@
 function show(id) {
     document.getElementById('sky').setAttribute('src', '#' + id);
   }
+
+function showtext(id,text)
+{
+  document.getElementById(id).setAttribute('value',text);
+}
+
+
