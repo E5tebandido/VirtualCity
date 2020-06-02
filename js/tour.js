4d7b2bf7655951d953b24e8d30a55bda8ea4e51f
Esteban Posada <aepr_18_@hotmail.com>
2020-06-01 19:10:00 -0500

    complete prepositions module

--- js/tour.js
+++ js/tour.js
@@ -1,11 +1,42 @@
 
-function show(id) {
-    document.getElementById('sky').setAttribute('src', '#' + id);
+function show(id) 
+  {
+    try
+    {
+      document.getElementById('sky').setAttribute('src', '#' + id);
+    }catch(error)
+    {
+      console.log(error)
+    }
   }
 
 function showtext(id,text)
 {
-  document.getElementById(id).setAttribute('value',text);
+  try
+  {
+    document.getElementById(id).setAttribute('value',text);
+  }catch(error)
+  {
+    console.log(error)
+  }
 }
+
+function crearFigura(tipofigura,color,escala,posicion)
+{
+  try
+    {
+      var scene = document.querySelector('a-scene');
+      var entity = document.createElement('a-entity');
+      entity.setAttribute('material','color', color);
+      entity.setAttribute('scale', escala);
+      entity.setAttribute('geometry', 'primitive',tipofigura);
+      entity.setAttribute('position', posicion);
+      scene.appendChild(entity);    
+  }catch(error)
+  {
+    console.log(error)
+  }
+}
+
 
 
