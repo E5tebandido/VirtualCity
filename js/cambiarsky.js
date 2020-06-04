4a7de7e8dbd9fec3a7f75913bc95d7f8eee1dc44
Esteban Posada <aepr_18_@hotmail.com>
2020-06-04 00:43:11 -0500

    dinamic changes in marine animals

--- js/cambiarsky.js
+++ js/cambiarsky.js
@@ -1,96 +1,42 @@
-var scene = null;
-var newentity = null;
-
-
 function show(skyid) 
   {
-    try
+    let url;
+    document.getElementById('sky').setAttribute('src', '#' + skyid);
+    if(document.getElementById('sky').getAttribute('src')=='#artic')
     {
-      setTimeout(function()
-      {
-          document.getElementById('sky').setAttribute('src', '#' + skyid);
-          if(document.getElementById('sky').getAttribute('src')=='#artic'){
-            try
-            {
-              if(scene == null || newentity == null)
-              {
-                $.getJSON("json/articdata.json",function(json)
-                {
-
-                  console.log("json");
-                    let articobjects = json.artic;
-                    for(i in articobjects)
-                    {
-                      console.log(articobjects[i].gltfmodel);
-                      scene = document.querySelector('a-scene');
-                      newentity = document.createElement('a-entity');
-                      newentity.setAttribute('gltf-model',articobjects[i].gltfmodel);
-                      newentity.setAttribute('scale', '0.01 0.01 0.01');
-                      newentity.setAttribute('position','-2 2 1.5');
-                      newentity.setAttribute('rotation', '0 -90 0');
-                      scene.appendChild(newentity);
-                      console.log(scene.appendChild(newentity));
-                    }
-                });
-                
-                
-              }else
-              {
-                scene.removeChild(newentity);
-                scene = document.querySelector('a-scene');
-                newentity = document.createElement('a-entity');
-                newentity.setAttribute('gltf-model','#fish');
-                newentity.setAttribute('scale', '0.01 0.01 0.01');
-                newentity.setAttribute('position','-2 2 1.5');
-                newentity.setAttribute('rotation', '0 -90 0');
-                scene.appendChild(newentity);
-                console.log(scene.appendChild(newentity));
-              }
-              
-            }catch(error)
-          {
-            console.log(error)
-          }
-          }else{
-            try
-            {
-              if(scene == null || newentity == null)
-              {
-                scene = document.querySelector('a-scene');
-                newentity = document.createElement('a-entity');
-                newentity.setAttribute('gltf-model','#octopus');
-                newentity.setAttribute('scale', '0.1 0.1 0.1');
-                newentity.setAttribute('position','-2 2 1.5');
-                newentity.setAttribute('rotation', '0 -90 0');
-                scene.appendChild(newentity);
-                console.log(scene.appendChild(newentity));
-              }else
-              {
-                scene.removeChild(newentity);
-                scene = document.querySelector('a-scene');
-                newentity = document.createElement('a-entity');
-                newentity.setAttribute('gltf-model','#octopus');
-                newentity.setAttribute('scale', '0.1 0.1 0.1');
-                newentity.setAttribute('position','-2 2 1.5');
-                newentity.setAttribute('rotation', '0 -90 0');
-                scene.appendChild(newentity);
-                console.log(scene.appendChild(newentity));
-              }  
-            }catch(error)
-          {
-            console.log(error)
-          }
-          }
-      }, 
-      3000
-      )
-    }catch(error)
+      url = "js/json/articdata.json"; 
+    }else
     {
-      console.log(error)
+      url = "js/json/pacificdata.json";
     }
+    $.getJSON(url,function(data)
+    {
+        let scene = document.querySelector('a-scene');
+        let modeldata = data.models;
+        let entities = document.getElementsByClassName('removable');
+        Array.from(entities).forEach(function(item)
+        {
+          item.remove();
+        })
+        for(i in modeldata)
+        {
+          createModel(modeldata[i], scene);
+        }
+    });
+  }
+
+  function createModel(data, scene)
+  {
+    console.log(data.gltfmodel);
+    entity = document.createElement('a-entity');
+    entity.setAttribute('class','removable');
+    entity.setAttribute('gltf-model',data.gltfmodel);
+    entity.setAttribute('scale', data.scale);
+    entity.setAttribute('position',data.position);
+    entity.setAttribute('rotation', data.rotation);
+    scene.appendChild(entity);
   }
 
 
-
-
+  
 
--- js/crearentidad.js
+++ js/crearentidad.js
@@ -1,24 +1,13 @@
-function crearFigura(id,tipofigura,color,escala,posicion)
+function crearFigura(tipofigura,color,escala,posicion)
 {
-  try
-    {
-      var scene = document.querySelector('a-scene');
-      var newentity = document.createElement('a-entity');
-      console.log(scene,newentity);
-      newentity.setAttribute('id',id);
-      newentity.setAttribute('material','color', color);
-      newentity.setAttribute('scale', escala);
-      newentity.setAttribute('geometry', 'primitive',tipofigura);
-      newentity.setAttribute('position', posicion);
-      scene.appendChild(newentity);
-      console.log(newentity.id);
-      setTimeout(function(){
-        if ($("#"+newentity.id).length > 0) {
-          $("#"+newentity.id).remove();
-        }
-      }, 5000)
-    }catch(error)
-  {
-    console.log(error)
-  }
+  let scene = document.querySelector('a-scene');
+  let newentity = document.createElement('a-entity');
+  newentity.setAttribute('material','color', color);
+  newentity.setAttribute('scale', escala);
+  newentity.setAttribute('geometry', 'primitive',tipofigura);
+  newentity.setAttribute('position', posicion);
+  scene.appendChild(newentity);
+  setTimeout(function(){
+    newentity.remove();
+  }, 5000)
 }
