f8e85b89cb256c9f31cc83ca77341d3d20bf4508
Esteban Posada <aepr_18_@hotmail.com>
2020-06-04 17:49:51 -0500

    added description of marine animals 3

--- js/cargapagina.js
+++ js/cargapagina.js
@@ -2,18 +2,22 @@ $(document).ready(function()
 {
     $("#ejerciciouno").on("click",function(event)
     {
+        document.getElementById('carouselExampleFade').remove();
         cargarTematica("contenedorejercicio","animalesmarinos.html")
     })
     $("#ejerciciodos").on("click",function(event)
     {
+        document.getElementById('carouselExampleFade').remove();
         cargarTematica("contenedorejercicio","vecindario.html")
     })
     $("#ejerciciotres").on("click",function(event)
     {
+        document.getElementById('carouselExampleFade').remove();
         cargarTematica("contenedorejercicio","baila.html")
     })
     $("#ejerciciocuatro").on("click",function(event)
     {
+        document.getElementById('carouselExampleFade').remove();
         cargarTematica("contenedorejercicio","preposiciones.html")
     })
     
